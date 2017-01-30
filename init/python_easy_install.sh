58bac9ac6a1c23d1cdeeaa0c9d21fc0d2b22f42f
Umi Uyura <umiuyura@gmail.com>
2017-01-30 16:03:28 +0900

    [Python] Python 3.3で導入されるユニットテスト用ライブラリMockのバックポートを追加

--- init/python_easy_install.sh
+++ init/python_easy_install.sh
@@ -6,9 +6,10 @@ easy_install-2.7 pip
 # use pip
 pip install flake8
 pip install pylint
+pip install mock              # 3.3 unittest.mock backport
 pip install numpy
 pip install pandas
 pip install python-dateutil
 pip install pytz
-pip install enum34
+pip install enum34            # 3.4 Enum backport
 pip install jsbeautifier
