10fd3c866cba118e50703a4099b4085b0146bdb0
Umi Uyura <umiuyura@gmail.com>
2017-07-14 19:38:28 +0900

    [Heroku] heroku-pipelinesプラグインを追加

--- init/heroku-plugins.sh
+++ init/heroku-plugins.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 heroku plugins:install heroku-accounts
-heroku plugins:install heroku-repo
+heroku plugins:install heroku-pipelines
 heroku plugins:install heroku-config
+heroku plugins:install heroku-repo
