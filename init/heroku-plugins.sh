3be61ba73c82d4e22966ab34627fec8bfa989131
Umi Uyura <umiuyura@gmail.com>
2018-07-12 11:39:59 +0900

    [Heroku] heroku-autocomplete導入

--- init/heroku-plugins.sh
+++ init/heroku-plugins.sh
@@ -4,5 +4,8 @@ heroku plugins:install heroku-accounts
 heroku plugins:install heroku-pipelines
 heroku plugins:install heroku-config
 heroku plugins:install heroku-repo
+heroku plugins:install heroku-autocomplete
 heroku plugins:install ynab/heroku-fork
 
+# Enable autocomplete
+heroku autocomplete
