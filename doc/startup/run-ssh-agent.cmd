b10740086e4efd05758e81b723e70fdcc56b560b
Umi Uyura <umiuyura@gmail.com>
2018-07-12 11:28:50 +0900

    Cygwin版ssh-agentを使うようにする
    Windows版OpenSSHが正式リリースされ、Windows版ssh-agentが標準導入されたため

--- doc/startup/run-ssh-agent.cmd
+++ doc/startup/run-ssh-agent.cmd
@@ -7,7 +7,7 @@ rem Press Win + R and run "shell:startup" to open the startup folder.
 rem
 
 rem Start ssh-agent in the background. It will not be closed
-start /b ssh-agent > %GNUPACK_HOME%\.ssh-agent-env
+start /b %GNUPACK_ROOT%\app\cygwin\cygwin\bin\ssh-agent > %GNUPACK_HOME%\.ssh-agent-env
 echo We need to ask you the password with which the SSH key is encoded
 echo After that we will update the environment variable so that you
 echo do not need to enter it again
