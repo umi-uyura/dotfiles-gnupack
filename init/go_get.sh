8c58e19a10db2837ea063a69f500ae0fb8e5b337
Umi Uyura <umiuyura@gmail.com>
2018-10-31 11:57:23 +0900

    [Go] GitHubキーワード検索ツールghkw、Slackダンプツールslack-dumpを追加

---
+++ init/go_get.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+go get -u github.com/kyoshidajp/ghkw      # Search how many keywords in GitHub
+go get -u github.com/PyYoshi/slack-dump   # Export History For Channel, Private Group and Direct Message From Slack
