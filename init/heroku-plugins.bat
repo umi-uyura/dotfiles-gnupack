e875dc8e4347adb333ddd2f3441f64b3c3cc165d
Umi Uyura <umiuyura@gmail.com>
2018-10-31 10:37:52 +0900

    [Heroku] Herokuアプリ同士の環境を比較するプラグインheroku-cli-diffを追加

--- init/heroku-plugins.bat
+++ init/heroku-plugins.bat
@@ -4,6 +4,7 @@ call heroku plugins:install heroku-accounts
 call heroku plugins:install heroku-config
 call heroku plugins:install heroku-repo
 rem heroku plugins:install heroku-autocomplete
+call heroku plugins:install heroku-cli-diff
 call heroku plugins:install ynab/heroku-fork
 
 rem Enable autocomplete, but Windows not support
