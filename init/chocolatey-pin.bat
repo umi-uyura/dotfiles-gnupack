86b5eeff4fbe057c136fcea981acf1a075df4c73
Umi Uyura <umiuyura@gmail.com>
2019-05-17 19:39:33 +0900

    [Chocolatey] ピン留め対象にAndroid Studioを追加

--- init/chocolatey-pin.bat
+++ init/chocolatey-pin.bat
@@ -1,7 +1,8 @@
 @echo off
 
+choco pin add -n=androidstudio
 choco pin add -n=GoogleChrome
-choco pin add -n=intellijidea-community
 choco pin add -n=heroku-cli
+choco pin add -n=intellijidea-community
 
 choco pin list
