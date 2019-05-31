b3c527b88f9cf7213bfcf964d5e075afabf6ad5d
Umi Uyura <umiuyura@gmail.com>
2019-05-31 12:52:57 +0900

    [Chocolatey] ピン留め対象にPostmanを追加

--- init/chocolatey-pin.bat
+++ init/chocolatey-pin.bat
@@ -4,6 +4,7 @@ choco pin add -n=androidstudio
 choco pin add -n=GoogleChrome
 choco pin add -n=heroku-cli
 choco pin add -n=intellijidea-community
+choco pin add -n=postman
 choco pin add -n=vscode
 
 choco pin list
