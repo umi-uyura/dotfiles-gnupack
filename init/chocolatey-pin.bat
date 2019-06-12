0f079e022cc470275dc197919c0f52c085194c90
Umi Uyura <umiuyura@gmail.com>
2019-06-12 10:25:55 +0900

    [Chocolatey] ピン留め対象にInsomniaを追加

--- init/chocolatey-pin.bat
+++ init/chocolatey-pin.bat
@@ -4,6 +4,7 @@ choco pin add -n=androidstudio
 choco pin add -n=Firefox
 choco pin add -n=GoogleChrome
 choco pin add -n=heroku-cli
+choco pin add -n=insomnia-rest-api-client
 choco pin add -n=intellijidea-community
 choco pin add -n=postman
 choco pin add -n=vscode
