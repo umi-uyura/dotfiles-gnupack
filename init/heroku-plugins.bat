49f201d59b9e353b4ac183461d8eaab03322be8a
Umi Uyura <umiuyura@gmail.com>
2021-06-02 12:26:08 +0900

    [Heroku] 月間の費用を表示するプラグインheroku-billingを追加

--- init/heroku-plugins.bat
+++ init/heroku-plugins.bat
@@ -6,6 +6,7 @@ call heroku plugins:install heroku-repo
 rem heroku plugins:install heroku-autocomplete
 call heroku plugins:install heroku-cli-diff
 call heroku plugins:install ynab/heroku-fork
+call heroku plugins:install @hyperoslo/heroku-billing
 
 rem Enable autocomplete, but Windows not support
 rem heroku autocomplete
