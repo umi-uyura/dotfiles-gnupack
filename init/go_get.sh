3384dc0bd506197de744e627dc06e9e5431395b5
Umi Uyura <umiuyura@gmail.com>
2021-06-02 12:51:50 +0900

    [golang] チートシート管理ツールcheatを追加

--- init/go_get.sh
+++ init/go_get.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+go get -u github.com/cheat/cheat/cmd/cheat    # Create and view interactive cheat sheets on the command-line
 go get -u github.com/kyoshidajp/ghkw      # Search how many keywords in GitHub
 go get -u github.com/PyYoshi/slack-dump   # Export History For Channel, Private Group and Direct Message From Slack
