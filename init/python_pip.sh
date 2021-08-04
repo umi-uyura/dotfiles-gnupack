49d45398152f84efbc566f85c228356fadacea32
Umi Uyura <umiuyura@gmail.com>
2021-08-04 19:00:23 +0900

    [Python] Cygwinパッケージから入れるもの

--- init/python_pip.sh
+++ init/python_pip.sh
@@ -12,3 +12,12 @@ pip3 install python-dateutil
 pip3 install pytz
 # pip3 install requests         # -> apt-cyg install python36-requests
 pip3 install pip-review
+
+
+pip2 install flake8
+# pip2 install pylint           # -> apt-cyg install pylint
+pip2 install python-dateutil
+# pip2 install pytz             # -> apt-cyg install python27-pytz
+# pip2 install requests         # -> apt-cyg install python27-requests
+pip2 install pathlib
+pip2 install pip-review
